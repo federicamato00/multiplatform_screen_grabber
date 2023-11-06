a815491935b046101138e4577de6712540e70255
federicamato00 <134133171+federicamato00@users.noreply.github.com>
2023-11-06 18:46:49 +0100

    close_window updated

---
+++ function.rs
@@ -0,0 +1,201 @@
+use druid::{MouseEvent, Point};
+
+use crate::drawing_area::{self, AppData, DragHandle};
+
+pub(crate) fn some_fields_are_equal(data: &drawing_area::AppData) -> bool {
+    if (data.start_image_modifier == data.save_image_modifier
+        && data.start_image_key == data.save_image_key)
+        || (data.start_image_modifier == data.restart_app_modifier
+            && data.start_image_key == data.restart_app_key)
+        || (data.start_image_modifier == data.restart_format_app_modifier
+            && data.start_image_key == data.restart_format_app_key)
+        || (data.start_image_modifier == data.edit_image_modifier
+            && data.start_image_key == data.edit_image_key)
+        || (data.start_image_modifier == data.quit_app_modifier
+            && data.start_image_key == data.quit_app_modifier)
+        || (data.save_image_modifier == data.quit_app_modifier
+            && data.save_image_key == data.quit_app_modifier)
+        || (data.save_image_modifier == data.restart_format_app_modifier
+            && data.save_image_key == data.restart_format_app_key)
+        || (data.save_image_modifier == data.restart_app_modifier
+            && data.save_image_key == data.restart_app_key)
+        || (data.save_image_modifier == data.edit_image_modifier
+            && data.save_image_key == data.edit_image_key)
+        || (data.quit_app_modifier == data.edit_image_modifier
+            && data.quit_app_key == data.edit_image_key)
+        || (data.quit_app_modifier == data.restart_app_modifier
+            && data.quit_app_key == data.restart_app_key)
+        || (data.quit_app_modifier == data.restart_format_app_modifier
+            && data.quit_app_key == data.restart_format_app_key)
+        || (data.edit_image_modifier == data.restart_app_modifier
+            && data.edit_image_key == data.restart_app_key)
+        || (data.restart_app_modifier == data.restart_format_app_modifier
+            && data.restart_app_key == data.restart_format_app_key)
+        || (data.edit_image_modifier == data.restart_format_app_modifier
+            && data.edit_image_key == data.restart_format_app_key)
+    {
+        true
+    } else {
+        false
+    }
+}
+
+pub(crate) fn are_all_fields_completed(data: &drawing_area::AppData) -> bool {
+    if (data.save_image_modifier != "None".to_string() || data.save_image_key != "".to_string())
+        && (data.edit_image_modifier != "None".to_string() || data.edit_image_key != "".to_string())
+        && (data.quit_app_key != "".to_string() || data.quit_app_modifier != "None".to_string())
+        && (data.start_image_key != "".to_string()
+            || data.start_image_modifier != "None".to_string())
+        && (data.restart_app_modifier != "None".to_string()
+            || data.restart_app_key != "".to_string())
+        && (data.restart_format_app_modifier != "None".to_string()
+            || data.restart_format_app_key != "".to_string())
+    {
+        true
+    } else {
+        false
+    }
+}
+
+// pub(crate) fn save_screen(data: &mut drawing_area::AppData, size: Size, save: bool) {
+//     if let (Some(start), Some(end)) = (data.start_position, data.end_position) {
+//         data.is_dragging = false;
+
+//         let name = data.label.clone();
+//         let format = data.radio_group;
+//         let size_clone = Arc::new(Mutex::new(None));
+//         // Calcola il rettangolo selezionato
+//         let rect = druid::Rect::from_points(start, end);
+
+//         // Chiama la funzione per catturare lo screenshot
+
+//         let start_position = Arc::new(Mutex::new(None));
+//         let end_position = Arc::new(Mutex::new(None));
+//         //println!("Selected area: {:?}, {:?}", (rect.x0, rect.y0), (rect.x1, rect.y1));
+//         // Crea un thread separato per catturare lo screenshot
+
+//         let screenshot_thread = thread::spawn(move || {
+//             // Imposta i dati di trascinamento per iniziare la cattura
+//             let end_position_clone_2 = Arc::clone(&end_position);
+//             let start_position_clone_3 = Arc::clone(&start_position);
+
+//             // Cattura uno screenshot.
+//             let size_clone2 = Arc::clone(&size_clone);
+//             *size_clone2.lock().unwrap() = Some(size);
+
+//             *start_position_clone_3.lock().unwrap() = Some((rect.x0, rect.y0));
+//             *end_position_clone_2.lock().unwrap() = Some((rect.x1, rect.y1));
+//             //println!("Selected area: {:?}, {:?}", (*start_position_clone_3.lock().unwrap()), *end_position_clone_2.lock().unwrap() );
+//             // Chiama la funzione di cattura screenshot
+//             //println!("wid: {:?}, {:?}",size.width,size.height);
+
+//             _ = screenshot::screen(
+//                 format,
+//                 start_position_clone_3,
+//                 end_position_clone_2,
+//                 name,
+//                 save,
+//             );
+//         });
+
+//         // Attendi la fine del thread di cattura screenshot
+//         screenshot_thread.join().unwrap();
+
+//         data.is_dragging = false;
+//         data.is_selecting = false;
+//         data.modify = false;
+//         data.is_found = false;
+//         data.hide_buttons = false;
+//         data.save = false;
+//         //println!("{:?}",data.last_key_event);
+//     }
+// }
+
+pub(crate) fn edit_rect(
+    handle: &DragHandle,
+    pos: Point,
+    data: &mut AppData,
+    mouse_event: &MouseEvent,
+) {
+    match handle {
+        DragHandle::TopLeft => {
+            data.rect.x0 = mouse_event.pos.x;
+            data.rect.y0 = mouse_event.pos.y;
+            // let pos = ctx.to_screen(druid::Point::new(mouse_event.pos.x, mouse_event.pos.y));
+
+            let coord = druid::Point { x: pos.x, y: pos.y };
+            data.start_position_to_display = Some(druid::Point {
+                x: mouse_event.pos.x,
+                y: mouse_event.pos.y,
+            });
+            data.start_position = Some(coord);
+            data.is_selecting = true;
+
+            //println!("{:?}, {:?}",data.start_position,data.end_position);
+        }
+        DragHandle::BottomRight => {
+            data.rect.x1 = mouse_event.pos.x;
+            data.rect.y1 = mouse_event.pos.y;
+            // let pos = ctx.to_screen(druid::Point::new(mouse_event.pos.x, mouse_event.pos.y));
+
+            let coord = druid::Point { x: pos.x, y: pos.y };
+            data.end_position_to_display = Some(druid::Point {
+                x: mouse_event.pos.x,
+                y: mouse_event.pos.y,
+            });
+            data.end_position = Some(coord);
+            data.is_selecting = true;
+        }
+        DragHandle::BottomLeft => {
+            data.rect.x0 = mouse_event.pos.x;
+            data.rect.y1 = mouse_event.pos.y;
+            // let pos = ctx.to_screen(druid::Point::new(mouse_event.pos.x, mouse_event.pos.y));
+
+            let coord = druid::Point {
+                x: data.end_position.unwrap().x,
+                y: pos.y,
+            };
+            data.end_position_to_display = Some(druid::Point {
+                x: data.end_position_to_display.unwrap().x,
+                y: mouse_event.pos.y,
+            });
+            data.end_position = Some(coord);
+            let coord = druid::Point {
+                x: pos.x,
+                y: data.start_position.unwrap().y,
+            };
+            data.start_position_to_display = Some(druid::Point {
+                x: data.rect.x0,
+                y: data.start_position_to_display.unwrap().y,
+            });
+            data.start_position = Some(coord);
+            data.is_selecting = true;
+        }
+        DragHandle::TopRight => {
+            data.rect.x1 = mouse_event.pos.x;
+            data.rect.y0 = mouse_event.pos.y;
+            // let pos = ctx.to_screen(druid::Point::new(mouse_event.pos.x, mouse_event.pos.y));
+
+            let coord = druid::Point {
+                x: pos.x,
+                y: data.end_position.unwrap().y,
+            };
+            data.end_position_to_display = Some(druid::Point {
+                x: mouse_event.pos.x,
+                y: data.end_position_to_display.unwrap().y,
+            });
+            data.end_position = Some(coord);
+            let coord = druid::Point {
+                x: data.start_position.unwrap().x,
+                y: pos.y,
+            };
+
+            data.start_position_to_display = Some(druid::Point {
+                x: data.start_position_to_display.unwrap().x,
+                y: data.rect.y0,
+            });
+            data.start_position = Some(coord);
+            data.is_selecting = true;
+        }
+    }
+}
---
+++ screenshot.rs
@@ -0,0 +1,291 @@
+use arboard::ImageData;
+use druid::Point;
+use image::{EncodableLayout, ImageBuffer, Rgba};
+use screenshots::{self, Screen};
+
+use crate::window_format::MyRadio;
+
+pub(crate) fn screen_new(
+    mut start_position: Point,
+    mut end_position: Point,
+) -> ImageBuffer<Rgba<u8>, Vec<u8>> {
+    let screen = Screen::from_point(0, 0).unwrap();
+    if start_position.x > end_position.x {
+        let prov_x0 = start_position.x;
+        start_position.x = end_position.x;
+        end_position.x = prov_x0;
+    }
+    if start_position.y > end_position.y {
+        let prov_y0 = start_position.y;
+        start_position.y = end_position.y;
+        end_position.y = prov_y0;
+    }
+    end_position.y = end_position.y - 2.1;
+    end_position.x = end_position.x - 2.1;
+    let image = screen
+        .capture_area(
+            (start_position.x + 2.1) as i32,
+            (start_position.y + 2.1) as i32,
+            ((end_position.x - start_position.x) - 1.) as u32,
+            ((end_position.y - start_position.y) - 1.) as u32,
+        )
+        .unwrap();
+    image
+}
+
+pub(crate) fn save_screen_new(
+    format: MyRadio,
+    name: String,
+    image: ImageBuffer<Rgba<u8>, Vec<u8>>,
+) {
+    let new_format = format;
+    let name_capture = name;
+    let form = match new_format {
+        MyRadio::Png => "png",
+        MyRadio::Jpeg => "jpeg",
+        MyRadio::Gif => "gif",
+    };
+    let mut myimage = image;
+    // println!("image: {:?}", myimage.width());
+    if myimage.width() == 0 && myimage.height() == 0 {
+        myimage = Screen::from_point(0, 0).unwrap().capture().unwrap();
+    }
+    myimage
+        .save(name_capture.to_owned() + "." + form.clone())
+        .unwrap();
+    // let name = name_capture.to_owned() + "." + form.clone();
+    let clipboard = &mut arboard::Clipboard::new().unwrap();
+
+    let bytes = myimage.as_bytes();
+    let img_data = ImageData {
+        width: myimage.width() as usize,
+        height: myimage.height() as usize,
+        bytes: bytes.as_ref().into(),
+    };
+    clipboard.set_image(img_data).unwrap();
+}
+// pub fn screen(
+//     format: MyRadio,
+//     start_position: Arc<Mutex<Option<(f64, f64)>>>,
+//     end_position: Arc<Mutex<Option<(f64, f64)>>>,
+//     name: String,
+//     save: bool,
+// ) -> ImageBuffer<Rgba<u8>, Vec<u8>> {
+//     let new_format = format;
+//     let name_capture = name;
+//     let mut image: ImageBuffer<Rgba<u8>, Vec<u8>> = ImageBuffer::new(0, 0);
+//     // altri sistemi operativi è l'unico che non da problemi di distorsione, controllare se su windows funziona ugualmente e in tal caso sostituire
+//     if let (Some((x1, y1)), Some((x2, y2))) = (
+//         *start_position.lock().unwrap(),
+//         *end_position.lock().unwrap(),
+//     ) {
+//         let form = match new_format {
+//             MyRadio::Png => "png",
+//             MyRadio::Jpeg => "jpeg",
+//             MyRadio::Bmp => "bmp",
+//         };
+//         let screen = Screen::from_point(0, 0).unwrap();
+
+//         image = screen
+//             .capture_area(
+//                 x1 as i32 + 1,
+//                 y1 as i32 + 1,
+//                 ((x2 - x1) - 1.5) as u32,
+//                 ((y2 - y1) - 1.5) as u32,
+//             )
+//             .unwrap();
+//         if save {
+//             image
+//                 .save(name_capture.to_owned() + "." + form.clone())
+//                 .unwrap();
+//             // let name = name_capture.to_owned() + "." + form.clone();
+//             let clipboard = &mut arboard::Clipboard::new().unwrap();
+
+//             let bytes = image.as_bytes();
+//             let img_data = ImageData {
+//                 width: image.width() as usize,
+//                 height: image.height() as usize,
+//                 bytes: bytes.as_ref().into(),
+//             };
+//             clipboard.set_image(img_data).unwrap();
+//         }
+//     }
+//     image
+// }
+
+// // Versione che fa post processing in un thread apparte. Al momento non funzionante
+// pub fn screen_thread( _is_dragging: Arc<Mutex<Option<bool>>>,mut capturer:Capturer,width: u32,height: u32,start_position:Arc<Mutex<Option<(f64, f64)>>>,end_position:Arc<Mutex<Option<(f64, f64)>>>)
+// {
+
+//     // Aspetta finché l'utente non ha finito di tracciare l'area dello schermo.
+
+//     let frame: Option<Vec<u8>> = loop {
+//         match capturer.frame() {
+
+//             /* DOPO L'AGGIUNTA DELLO STRIDE GLI SCREEN SONO SPOSTATI LEGGERMENTE IN ALTO, VEDI DI AGGIUSTARE*/
+//             Ok(frame) => {
+//                 // println!("Demtro frame");
+//                 // Calcola lo stride dell'immagine
+//                 let stride = frame.len() / height as usize;
+
+//                 // Crea una nuova immagine con le dimensioni corrette
+//                 let mut image = vec![0; stride * height as usize];
+
+//                 // Copia i dati dell'immagine, tenendo conto dello stride
+//                 for y in 0..height {
+//                     let src_off = (y * stride as u32) as usize;
+//                     let dst_off = (y * width * 4) as usize;
+//                     let src_slice = &frame[src_off..src_off + width as usize * 4];
+//                     let dst_slice = &mut image[dst_off..dst_off + width as usize * 4];
+//                     dst_slice.copy_from_slice(src_slice);
+//                 }
+//                     // Ritorna l'immagine modificata
+//                     break Some(image);
+//                 }
+//                     ,
+//             Err(e) if e.kind() == std::io::ErrorKind::WouldBlock => {
+//                 // Aspetta un po' per il prossimo frame.
+
+//                 thread::sleep(Duration::from_millis(100));
+//                 break None;
+//             }
+//             Err(e) => panic!("frame error: {}", e),
+//         }
+//     };
+
+//     if frame.is_none(){
+//         return;
+//     }
+
+//     let frame = frame.unwrap();
+
+//     thread::spawn(move ||{
+//                     // Converte il frame in un'immagine.
+//                 let mut image: ImageBuffer<Rgba<u8>, _> = ImageBuffer::from_raw(
+//                     width as u32,
+//                     height as u32,
+//                     frame.to_vec(),
+//                 ).unwrap();
+
+//                 for pixel in image.pixels_mut() {
+//                     let b = pixel[0];
+//                     let g = pixel[1];
+//                     let r = pixel[2];
+//                     let a = pixel[3];
+//                     *pixel = Rgba([r, g, b, a]);
+//                 }
+
+//             // println!("height: {:?}",height);
+//             // // Converte il frame in un'immagine.
+//             // let mut image: ImageBuffer<Rgba<u8>, _> = ImageBuffer::from_raw(
+//             //     width as u32,
+//             //     height as u32,
+//             //     frame.to_vec(),
+//             // ).unwrap();
+
+//             // for pixel in image.pixels_mut() {
+//             //     let b = pixel[0];
+//             //     let g = pixel[1];
+//             //     let r = pixel[2];
+//             //     let a = pixel[3];
+//             //     *pixel = Rgba([r, g, b, a]);
+//             // }
+
+//             let im=DynamicImage::from(image.clone());
+//             let _=DynamicImage::from(im).save("original.png");
+//             //println!("Image size: {:?}, {:?}", im.width(), im.height());
+//             // Ritaglia l'immagine all'area specificata.
+//             if let (Some((x1, y1)), Some((x2, y2))) = (*start_position.lock().unwrap(), *end_position.lock().unwrap()) {
+
+//                 let sub_image = (DynamicImage::from(image)).crop((x1+1.) as u32, (y1+1.) as u32, (x2-x1-1.5) as u32, (y2-y1-1.5) as u32);
+//                 //println!("{:?}, {:?} x1: {:?}, y1: {:?}", (x2-x1), (y2-y1),x1,y1);
+//                 match sub_image.save("screenshot_grabbed.png") {
+//                     Ok(_) => {println!("Successo!");return},
+//                     Err(e) if e.to_string().contains("Zero width not allowed") => {println!("Errore");return},
+//                     Err(_) => panic!("Unexpected error!"),
+//                 }
+
+//             }
+//     });
+
+// }
+
+// // Versione senza mutex, potrebbe essere rischiosa da usare
+// pub fn screen_no_mutex( _is_dragging:Option<bool>,mut capturer:Capturer,width: u32,height: u32,start_position:Option<(f64, f64)>,end_position:Option<(f64, f64)>)
+// {
+
+//  loop{
+
+//     // Aspetta finché l'utente non ha finito di tracciare l'area dello schermo.
+
+//     let frame: Option<Vec<u8>> = loop {
+//         match capturer.frame() {
+
+//             /* DOPO L'AGGIUNTA DELLO STRIDE GLI SCREEN SONO SPOSTATI LEGGERMENTE IN ALTO, VEDI DI AGGIUSTARE*/
+//             Ok(frame) => {
+//                 // println!("Demtro frame");
+//                 // Calcola lo stride dell'immagine
+//                 let stride = frame.len() / height as usize;
+
+//                 // Crea una nuova immagine con le dimensioni corrette
+//                 let mut image = vec![0; stride * height as usize];
+
+//                 // Copia i dati dell'immagine, tenendo conto dello stride
+//                 for y in 0..height {
+//                     let src_off = (y * stride as u32) as usize;
+//                     let dst_off = (y * width * 4) as usize;
+//                     let src_slice = &frame[src_off..src_off + width as usize * 4];
+//                     let dst_slice = &mut image[dst_off..dst_off + width as usize * 4];
+//                     dst_slice.copy_from_slice(src_slice);
+//                 }
+//                     // Ritorna l'immagine modificata
+//                     break Some(image);
+//                 }
+//                     ,
+//             Err(e) if e.kind() == std::io::ErrorKind::WouldBlock => {
+//                 // Aspetta un po' per il prossimo frame.
+
+//                 thread::sleep(Duration::from_millis(100));
+//                 break None;
+//             }
+//             Err(e) => panic!("frame error: {}", e),
+//         }
+//     };
+
+//     if frame.is_none(){
+//         continue;
+//     }
+
+//     let frame = frame.unwrap();
+
+//      // Converte il frame in un'immagine.
+//      let mut image: ImageBuffer<Rgba<u8>, _> = ImageBuffer::from_raw(
+//          width as u32,
+//          height as u32,
+//          frame.to_vec(),
+//      ).unwrap();
+
+//      for pixel in image.pixels_mut() {
+//          let b = pixel[0];
+//          let g = pixel[1];
+//          let r = pixel[2];
+//          let a = pixel[3];
+//          *pixel = Rgba([r, g, b, a]);
+//      }
+
+//     let im=DynamicImage::from(image.clone());
+//     let _=DynamicImage::from(im).save("original.png");
+//     //println!("Image size: {:?}, {:?}", im.width(), im.height());
+//     // Ritaglia l'immagine all'area specificata.
+//     if let (Some((x1, y1)), Some((x2, y2))) = (start_position, end_position) {
+//         let sub_image = (DynamicImage::from(image)).crop((x1) as u32, (y1) as u32, (x2-x1) as u32, (y2-y1) as u32);
+//         //println!("{:?}, {:?} x1: {:?}, y1: {:?}", (x2-x1), (y2-y1),x1,y1);
+//         match sub_image.save("screenshot_grabbed.png") {
+//             Ok(_) => break,
+//             Err(e) if e.to_string().contains("Zero width not allowed") => continue,
+//             Err(_) => panic!("Unexpected error!"),
+//         }
+
+//     }
+// }
+// }
